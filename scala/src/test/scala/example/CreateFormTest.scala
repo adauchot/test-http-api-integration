1259318aad0d337cf2a8ded8c7b57ae87f14687d
aurelien <aurelien.dauchot@soprism.com>
2024-03-04 13:25:05 +0100

    Added more tests

--- scala/src/test/scala/example/CreateFormTest.scala
+++ scala/src/test/scala/example/CreateFormTest.scala
@@ -3,7 +3,7 @@ package example
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
-import com.example.CreateForm
+import com.example.{CreateForm, CreateFormFailure}
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock._
 import data.TestData
@@ -18,7 +18,11 @@ class CreateFormTest extends AnyFlatSpec with BeforeAndAfterAll {
   implicit private val actorMaterializer: ActorMaterializer = ActorMaterializer()
   implicit private val executionContext: ExecutionContextExecutor = actorSystem.dispatcher
 
+  // Setting up global test data
   private val formId = TestData.formId
+  private val typeformBaseUrl = TestData.typeformBaseUrl
+  private val typeformAccessToken = TestData.typeformAccessToken
+  private val typeformWorkspace = TestData.typeformWorkspace
 
   private val wiremockServer = new WireMockServer(5001)
 
@@ -26,13 +30,11 @@ class CreateFormTest extends AnyFlatSpec with BeforeAndAfterAll {
     wiremockServer.start()
     configureFor("localhost", 5001)
   }
-  override def afterAll(): Unit = wiremockServer.stop()
-
+  override def afterAll(): Unit = {
+    wiremockServer.stop()
+  }
 
   "CreateForm" should "creates a form successfully" in {
-    val typeformBaseUrl = TestData.typeformBaseUrl
-    val typeformAccessToken = TestData.typeformAccessToken
-    val typeformWorkspace = TestData.typeformWorkspace
     val requestBody = TestData.requestBody
 
     stubFor(post(urlEqualTo("/forms"))
@@ -77,5 +79,53 @@ class CreateFormTest extends AnyFlatSpec with BeforeAndAfterAll {
 
     val result = createForm.execute
     assert(result.isLeft)
+  }
+
+  "CreateForm" should "return an error when the location header is not present" in {
+    stubFor(post(urlEqualTo("/forms"))
+      .willReturn(aResponse()
+        .withStatus(201)
+      )
+    )
+
+    val createForm = CreateForm(
+      typeformBaseUrl,
+      typeformAccessToken,
+      typeformWorkspace,
+      Http()
+    )
+
+    val result = createForm.execute
+    assert(result.isLeft)
+    assert(result.left.forall(_ == CreateFormFailure.LocationHeaderNotPresent))
+  }
+
+  "CreateForm" should "return an error when the url is invalid" in {
+    val typeformBaseUrl = "invalid-url"
+
+    val createForm = CreateForm(
+      typeformBaseUrl,
+      typeformAccessToken,
+      typeformWorkspace,
+      Http()
+    )
+
+    val result = createForm.execute
+    assert(result.isLeft)
+    assert(result.left.forall(_ == CreateFormFailure.InvalidUrl))
+  }
+
+  "CreateForm" should "return an error when the request is not executed" in {
+    // I don't really like putting null but it was effective and I couldn't think of a better way to do it
+    val createForm = CreateForm(
+      null,
+      typeformAccessToken,
+      typeformWorkspace,
+      Http()
+    )
+
+    val result = createForm.execute
+    assert(result.isLeft)
+    assert(result.left.forall(_.isInstanceOf[CreateFormFailure.RequestNotExecuted]))
   }
 }
